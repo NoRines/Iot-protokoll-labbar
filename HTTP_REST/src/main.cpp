edf62f4244def832c6e274252895ef0c175aded4
Markus Fält <markus.falt@gmail.com>
2019-11-08 12:35:11 +0100

    win socket class
    Tror den funkar måste bara dubbelkolla med wireshark att den gör som jag tror.

--- HTTP_REST/src/main.cpp
+++ HTTP_REST/src/main.cpp
@@ -1,10 +1,74 @@
 #include <iostream>
 #include <memory>
-#include "unix_socket.h"
-#include <string.h>
+#include "win_socket.h"
+
+
+#include <Ws2tcpip.h>
+
+
+// Link with ws2_32.lib
+#pragma comment(lib, "Ws2_32.lib")
 
 int main(int argc, char** argv)
 {
+#ifdef _WIN32
+	// Init winsock
+	WSADATA wsaData;
+
+	// Initialize Winsock
+	int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+	try
+	{
+		std::unique_ptr<Socket> serverSock(new WinSocket(SocketType::STREAM));
+
+		serverSock->bind("0.0.0.0", 54000);
+
+		serverSock->listen();
+		std::cout << "Listening..." << std::endl;
+
+		std::unique_ptr<Socket> clientSock(serverSock->accept());
+
+		auto clientAddress = clientSock->getAddress();
+		std::cout << clientAddress.host << " : " << clientAddress.port << std::endl;
+
+		serverSock->close();
+
+		char buf[4096];
+		while (true)
+		{
+			memset(buf, 0, 4096);
+			int bytesRecived = clientSock->receive(buf, 4096);
+
+			if (bytesRecived == 0)
+			{
+				std::cout << "Dissconnected" << std::endl;
+				break;
+			}
+
+			std::cout << "Recived> " << std::string(buf, 0, bytesRecived) << std::endl;
+
+			clientSock->send(std::string(buf, 0, bytesRecived));
+		}
+
+		clientSock->shutdown(SocketShutdownType::RDWR);
+		clientSock->close();
+	}
+	catch (const char* errorMsg)
+	{
+		std::cerr << errorMsg << std::endl;
+		return 1;
+	}
+
+	// Shutdown winsock
+	WSACleanup();
+
+#endif
+#ifdef __linux__
 	try
 	{
 		std::unique_ptr<Socket> serverSocket(new UnixSocket(SocketType::STREAM));
@@ -44,6 +108,6 @@ int main(int argc, char** argv)
 		std::cout << error << std::endl;
 		return 1;
 	}
-
+#endif
 	return 0;
 }
---
+++ HTTP_REST/src/win_socket.cpp
@@ -0,0 +1,143 @@
+#ifdef _WIN32
+
+#include "win_socket.h"
+
+WinSocket::WinSocket(SOCKET s) : Socket(), sock(s)
+{}
+
+WinSocket::WinSocket(SocketType type) : Socket()
+{
+	sock = INVALID_SOCKET;
+
+	sock = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		throw std::string("creating socket failed: %d\n", WSAGetLastError()).c_str();
+	}
+}
+
+WinSocket::~WinSocket()
+{
+	close();
+}
+
+void WinSocket::connect(const std::string& address, int port)
+{
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	inet_pton(AF_INET, address.c_str(), &addr.sin_addr);
+
+	int bindRes = ::connect(sock, (sockaddr*)&addr, sizeof(addr));
+	if (bindRes == SOCKET_ERROR)
+	{
+		throw std::string("connect failed with error %d\n", WSAGetLastError()).c_str();
+	}
+}
+
+void WinSocket::bind(const std::string& address, int port)
+{
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	inet_pton(AF_INET, address.c_str(), &addr.sin_addr);
+
+	int bindRes = ::bind(sock, (sockaddr*)&addr, sizeof(addr));
+	if (bindRes == SOCKET_ERROR)
+	{
+		throw std::string("bind failed with error %d\n", WSAGetLastError()).c_str();
+	}
+}
+
+void WinSocket::listen()
+{
+	int listenRes = ::listen(sock, SOMAXCONN);
+	if (listenRes == SOCKET_ERROR)
+	{
+		throw std::string("listen failed with error %d\n", WSAGetLastError()).c_str();
+	}
+}
+
+Socket* WinSocket::accept()
+{
+	SOCKET ClientSocket = ::accept(sock, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET)
+	{
+		throw std::string("accept failed with error %d\n", WSAGetLastError()).c_str();
+	}
+	return new WinSocket(ClientSocket);
+}
+
+int WinSocket::send(const std::string& msg)
+{
+	int bytesSent = ::send(sock, msg.c_str(), msg.size() + 1, 0);
+
+	if (bytesSent == SOCKET_ERROR)
+	{
+		throw std::string("send failed with error %d\n", WSAGetLastError()).c_str();
+	}
+
+	return bytesSent;
+}
+
+int WinSocket::receive(char* buf, int bufSize)
+{
+	int bytesRecived = recv(sock, buf, bufSize, 0);
+
+	if (bytesRecived == SOCKET_ERROR)
+	{
+		throw std::string("recv failed with error %d\n", WSAGetLastError()).c_str();
+	}
+
+	return bytesRecived;
+}
+
+void WinSocket::shutdown(SocketShutdownType type)
+{
+	int t = 0;
+	switch (type)
+	{
+	case SocketShutdownType::RD:
+		t = SD_RECEIVE;
+		break;
+	case SocketShutdownType::WR:
+		t = SD_SEND;
+		break;
+	case SocketShutdownType::RDWR:
+		t = SD_BOTH;
+		break;
+	}
+
+
+	int shutdownRes = ::shutdown(sock, t); // SD_SEND, SD_RECEIVE
+	if (shutdownRes == SOCKET_ERROR)
+	{
+		throw std::string("shutdown failed with error %d\n", WSAGetLastError()).c_str();
+	}
+}
+
+void WinSocket::close()
+{
+	closesocket(sock);
+}
+
+Address WinSocket::getAddress()
+{
+	sockaddr_in hint;
+	socklen_t addrSize = sizeof(hint);
+	int res = ::getsockname(sock, (sockaddr*)&hint, &addrSize);
+
+	if (res == SOCKET_ERROR)
+	{
+		throw std::string("Error getting address/name %d\n", WSAGetLastError()).c_str();
+	}
+
+	char buf[INET_ADDRSTRLEN];
+	memset(buf, 0, INET_ADDRSTRLEN);
+
+	::inet_ntop(AF_INET, &hint.sin_addr, buf, INET_ADDRSTRLEN);
+
+	return { std::string(buf), ntohs(hint.sin_port) };
+}
+
+#endif

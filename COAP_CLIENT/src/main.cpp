3eb95d3795a2946a496a489e7b854ab804030752
Markus Fält <markus.falt@gmail.com>
2019-11-25 17:25:51 +0100

    Testade enkla GET meddelanden till coap.me

--- COAP_CLIENT/src/main.cpp
+++ COAP_CLIENT/src/main.cpp
@@ -5,25 +5,36 @@
 #include <string>
 #include <cstring>
 
+#include <cstdint>
 
 int main(int argc, char** argv)
 {
 	try
 	{
-		std::unique_ptr<SocketInterface> serverSocket = std::make_unique<Socket>(SocketType::DGRAM);
+		// Test server is coap.me the which has ip-address: 134.102.218.18
 
 
+		// Create the socket
+		std::unique_ptr<SocketInterface> socket = std::make_unique<Socket>(SocketType::DGRAM);
+
+		// Very simple GET message
+		uint8_t simpleGet[] = { 0b01010000, 0b00000001, 0b10101010, 0b10101010 };
+
+		// Send the simple get to coap.me
+		std::cout << "Sending GET..." << std::endl;
+		int bytesSent = socket->sendTo((char*)simpleGet, 4, {"134.102.218.18", 5683});
+		std::cout << bytesSent << " bytes sent" << std::endl << std::endl;
+
+		// Wait for the response
+		std::cout << "Waiting for response..." << std::endl;
 		char buf[4096];
-		Address address = {"0.0.0.0", 54000};
+		memset(buf, 0, 4096);
+		Address receiveAddr = {"134.102.218.18", 5683};
+		int bytesReceived = socket->receiveFrom(buf, 4096, receiveAddr);
+		std::cout << bytesReceived << " bytes received" << std::endl << std::endl;
 
-		serverSocket->bind(address);
-
-		int bytesRecived = serverSocket->receiveFrom(buf, 4096, address);
-		
-		std::cout << address.host << " : " << address.port << std::endl;
-		std::cout << std::string(buf, 0, bytesRecived) << std::endl;
-
-		serverSocket->shutdown(SocketShutdownType::RDWR);
+		std::cout << "Raw message: " << std::endl;
+		std::cout << std::string(buf, 0, bytesReceived) << std::endl;
 	}
 	catch(std::string error)
 	{

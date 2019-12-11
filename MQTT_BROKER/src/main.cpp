b272557d090db431d5d6edb55a01b7c721ad3f77
Markus Fält <markus.falt@gmail.com>
2019-12-11 19:35:52 +0100

    La till ett alternativt sätt att parsa meddelanded på. Plus test mot HiveMQTT

--- MQTT_BROKER/src/main.cpp
+++ MQTT_BROKER/src/main.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
 #include <cstdint>
+#include <cstring>
 #include <array>
+#include <memory>
+#include <thread>
 
 #include "socket/socket.h"
+
+enum class ParseState
+{
+	HEADER = 0,
+	LENGTH = 1,
+	VAR_HEADER = 2,
+	PAYLOAD = 3
+};
+
+struct MqttParseData
+{
+	ParseState state = ParseState::HEADER;
+
+	uint8_t control = 0;
+	uint8_t flags = 0;
+
+	uint32_t bytesParsed = 0;
+
+	int lengthBytes = 0;
+	uint32_t messageLength = 0;
+};
+
+bool testTestMqtt(uint8_t currentByte, MqttParseData& parseData)
+{
+	switch(parseData.state)
+	{
+		case ParseState::HEADER:
+			{
+				parseData.control = currentByte >> 4;
+				parseData.flags = currentByte | 0x0F;
+
+				parseData.state = ParseState::LENGTH;
+				return true;
+			} break;
+		case ParseState::LENGTH:
+			{
+				parseData.messageLength |= (currentByte & 0x7f) << (7 * parseData.lengthBytes++);
+
+				if(currentByte == 0)
+					return false;
+				
+				if((currentByte & 0x80) == 0 || parseData.lengthBytes >= 4)
+					parseData.state = ParseState::VAR_HEADER;
+			} break;
+		case ParseState::VAR_HEADER:
+			{
+				parseData.bytesParsed++;
+				if(parseData.bytesParsed >= parseData.messageLength)
+					return false;
+			} break;
+		case ParseState::PAYLOAD:
+			{
+				parseData.bytesParsed++;
+				if(parseData.bytesParsed >= parseData.messageLength)
+					return false;
+			} break;
+	}
+
+	return true;
+}
 
 void testMQTT(uint8_t* msg) // TODO(markus): Refactor this maybe to a class or something.
 {
@@ -106,11 +169,64 @@ void testMQTT(uint8_t* msg) // TODO(markus): Refactor this maybe to a class or s
 	std::cout << std::endl << "Length value: " << length << std::endl;
 }
 
+void connHandler(SocketInterface* sock)
+{
+	std::unique_ptr<SocketInterface> clientSock(sock);
+
+	auto clientAddress = clientSock->getAddress();
+
+	std::cout << clientAddress.host << " : " << clientAddress.port << std::endl;
+
+	constexpr int bufLen = 1024;
+	char buf[bufLen];
+
+	int bytesParsed = 0;
+	MqttParseData parseData;
+
+	bool continueParsing = true;
+	do
+	{
+		memset(buf, 0, bufLen);
+		int bytesReceived = clientSock->receive(buf, bufLen);
+
+		while(continueParsing)
+		{
+			continueParsing = testTestMqtt(buf[bytesParsed++], parseData);
+		}
+	} while(continueParsing);
+
+
+	std::cout << "Message Type: " << (int)parseData.control << std::endl;
+	std::cout << "Message Length: " << parseData.messageLength << std::endl;
+
+	clientSock->shutdown(SocketShutdownType::RDWR);
+}
+
 int main(int argc, char** argv)
 {
-	uint8_t minMessage[] = { 0x20, 0xff, 0xff, 0xff, 0x7f };
+	//uint8_t test[] = { 0x20, 0x80, 0x80, 0x80, 0x01 };
+	//MqttParseData parseData;
 
-	testMQTT(minMessage);
+	//for(int i = 0; i < 5; i++)
+	//	std::cout << testTestMqtt(test[i], parseData) << std::endl;
+
+	//std::cout << parseData.messageLength << std::endl;
+
+	std::unique_ptr<SocketInterface> serverSock = std::make_unique<Socket>(SocketType::STREAM);
+
+	serverSock->bind({"0.0.0.0", 1883});
+
+	serverSock->listen();
+	std::cout << "Listening..." << std::endl;
+
+	while(1)
+	{
+		SocketInterface* sock = serverSock->accept();
+		std::thread handleConnection(connHandler, sock);
+		handleConnection.detach();
+	}
+
+	serverSock->shutdown(SocketShutdownType::RDWR);
 
 	return 0;
 }
